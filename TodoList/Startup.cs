2b408c9e444f0d524d9a9571df1d8d0d4c52f1d3
damoss08 <davidmoss01@gmail.com>
2018-10-30 13:19:06 -0500

    Adding JavaScript Todo code

---
+++ TodoList/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(TodoList.Startup))]
+
+namespace TodoList
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
